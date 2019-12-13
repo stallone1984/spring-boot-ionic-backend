2456379d1c07dea58356ab87c64b9645321ef81d
Heitor Bernardino <heitor.albano1984@gmail.com>
2019-12-13 14:16:23 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,15 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
+@ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-		StandardError se = new StandardError();
+		StandardError se = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(se);
 	}
 }
--- src/main/java/com/nelioalves/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/StandardError.java
@@ -2,4 +2,39 @@ package com.nelioalves.cursomc.resources.exceptions;
 
 public class StandardError {
 
+	private Integer status;
+	private String mensagem;
+	private Long timestamp;
+
+	public StandardError(Integer status, String mensagem, Long timestamp) {
+		super();
+		this.status = status;
+		this.mensagem = mensagem;
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
 }
