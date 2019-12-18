25c3971be8d987d28d05d33bcdb52b4121cf33b9
Heitor Bernardino <heitor.albano1984@gmail.com>
2019-12-18 17:05:32 -0300

    Pedido, Pagamento e Estado Pagamento

---
+++ src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nelioalves.cursomc.domain.Pedido;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
